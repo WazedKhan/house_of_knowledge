0eb9510fc26206af3a5895be41f9b8bfdbe4a410
WazedKhan <wazedkhan119399@gmail.com>
2023-09-16 19:10:56 +0600

    removed ASYNC_UNSAFE from setting

--- project/config/settings.py
+++ project/config/settings.py
@@ -114,4 +114,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-DJANGO_ALLOW_ASYNC_UNSAFE = True
