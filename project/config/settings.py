bdb07c8e53973f2a47c1c36d85ee12e8245c047f
WazedKhan <wazedkhan119399@gmail.com>
2023-09-23 20:43:34 +0600

    reverted to sqlite3

--- project/config/settings.py
+++ project/config/settings.py
@@ -71,11 +71,18 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        conn_max_age=600,
-        conn_health_checks=True,
-    ),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         conn_max_age=600,
+#         conn_health_checks=True,
+#     ),
+# }
 AUTH_USER_MODEL = "core.User"
 
 # Password validation
