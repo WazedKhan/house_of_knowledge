6b5ab544123579c21dd8ccb27a72d3e3d5debdf3
WazedKhan <wazedkhan119399@gmail.com>
2023-10-07 23:20:39 +0600

    book endpoint

--- project/book/rest/views/book.py
+++ project/book/rest/views/book.py
@@ -1,9 +1,12 @@
 from rest_framework.generics import ListCreateAPIView
 
+
+from auditlog.mixins import LogAccessMixin
+
 from book.models import Book
 from book.rest.serializers.book import BookSerializer
 
 
-class PublicBooks(ListCreateAPIView):
+class PublicBooks(LogAccessMixin, ListCreateAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.filter()
